11beb6c0241e036d7615dcbbfdd6e73d179aca54
AK <bibmaster@mail.ru>
2019-08-15 01:27:51 +0300

    addings

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 import slack
 
-client = slack.WebClient('xoxb-690139347445-728256943191-pH9k5clleGyApMddhpm28iBY')
+client = slack.WebClient('')
 
 response = client.chat_postMessage(
     channel='#test',
